2342ef3d757c3318b97af685d1fbf29802aa0e7f
www-data <www-data@bitey0asus.(none)>
2012-11-24 22:01:42 -0500

    Php info file

--- wordpress/wp-content/themes/pinboard/sidebar-boxes.php
+++
@@ -1,5 +0,0 @@
-<?php if( is_active_sidebar( 9 ) ) : ?>
-	<div id="sidebar-boxes" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 9 ) ; ?>
-	</div><!-- #sidebar-home -->
-<?php endif; ?>
--- wordpress/wp-content/themes/pinboard/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar" <?php pinboard_sidebar_class(); ?>>
-	<?php get_sidebar( 'top' ); ?>
-	<?php get_sidebar( 'left' ); ?>
-	<?php get_sidebar( 'right' ); ?>
-	<?php get_sidebar( 'bottom' ); ?>
-</div><!-- #sidebar -->
